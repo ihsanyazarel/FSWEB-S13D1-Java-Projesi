ea9ede61575f4856648bc1985170b9e689ec916e
İhsan Yazarel <ihsanyazarel@hotmail.com>
2023-07-24 14:29:09 +0300

    shouldWakeUp finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(shouldWakeUp(true, 1));
+        System.out.println(shouldWakeUp(false, 2));
+        System.out.println(shouldWakeUp(true, 8));
+        System.out.println(shouldWakeUp(true, -1));
+    }
+
+    public static boolean shouldWakeUp(boolean bark, int time) {
+        return (time < 24 && time > -1) && bark && (time > 20 || time < 8);
     }
 }
+
